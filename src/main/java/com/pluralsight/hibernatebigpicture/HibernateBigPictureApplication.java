2415b3a8c09ccb7810142f552ed9bb7de68bd6ba
Michael Hoffman <mike@michaelhoffmaninc.com>
2018-10-28 16:26:42 -0500

    Initial code commit:

---
+++ src/main/java/com/pluralsight/hibernatebigpicture/HibernateBigPictureApplication.java
@@ -0,0 +1,12 @@
+package com.pluralsight.hibernatebigpicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateBigPictureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateBigPictureApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pluralsight/hibernatebigpicture/HibernateBigPictureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pluralsight.hibernatebigpicture;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HibernateBigPictureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
